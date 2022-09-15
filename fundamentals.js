3d1f7cb64aa6d1730b08a9b913c23d374f667e36
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2022-09-15 11:48:41 +0100

    Dynamic Properties

--- fundamentals.js
+++ fundamentals.js
@@ -1,8 +1,16 @@
 const aCar = {
     owner : "Joe Bloggs",
     type : {make: "Toyota", model:"Corolla",cc:1.8},
-    registration : '201WD1058'
+    registration: {year:"201",countyCode:"WD",number:1058}
   };
   
+  aCar.mileage = 10000;
+  aCar.colour = {exteriorColour:"Red", interiorFabric:"Leather",
+    shade:"Cream"}
+
+
   console.log(aCar.owner + " drives a "+aCar.type.make);
-  
+  console.log(aCar.registration.year+"-"+aCar.registration.countyCode);
+  console.log("It is a "+aCar.colour.exteriorColour+" car, "+
+  aCar.mileage+" mileage, with "+aCar.colour.interiorFabric+
+  " interior.")
